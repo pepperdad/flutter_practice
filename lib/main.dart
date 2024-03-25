a254916428b84df26190b9801f327d87569f6394
jerry <becomegoodguy@gmail.com>
2024-03-25 23:49:11 +0900

    column 스크롤 기능

--- lib/main.dart
+++ lib/main.dart
@@ -18,42 +18,54 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.start, // ROW의 가운데 정렬
-          crossAxisAlignment: CrossAxisAlignment
-              .center, // Column과 마찬가지로 Container의 height를 줘야 정렬이 됨.
-          children: [
-            Container(
-              width: 100,
-              height: 80,
-              color: Colors.red,
-              child: const Text('Container 1'),
-            ),
-            Container(
-              width: 100,
-              height: 80,
-              color: Colors.blue,
-              child: const Text('Container 2'),
-            ),
-            Container(
-              width: 100,
-              height: 80,
-              color: Colors.green,
-              child: const Text('Container 3'),
-            ),
-          ],
-        ),
-        Container(
-          width: 300,
-          height: 120,
-          color: Colors.yellow,
-          child: const Text("Container 4"),
-        )
-      ],
+    return SingleChildScrollView(
+      // 화면 스크롤 가능
+      child: Column(
+        children: [
+          Container(
+            color: Colors.red,
+            width: double.infinity,
+            height: 100,
+            margin: const EdgeInsets.symmetric(vertical: 10),
+          ),
+          Container(
+            color: Colors.red,
+            width: double.infinity,
+            height: 100,
+            margin: const EdgeInsets.symmetric(vertical: 10),
+          ),
+          Container(
+            color: Colors.red,
+            width: double.infinity,
+            height: 100,
+            margin: const EdgeInsets.symmetric(vertical: 10),
+          ),
+          Container(
+            color: Colors.red,
+            width: double.infinity,
+            height: 100,
+            margin: const EdgeInsets.symmetric(vertical: 10),
+          ),
+          Container(
+            color: Colors.red,
+            width: double.infinity,
+            height: 100,
+            margin: const EdgeInsets.symmetric(vertical: 10),
+          ),
+          Container(
+            color: Colors.red,
+            width: double.infinity,
+            height: 100,
+            margin: const EdgeInsets.symmetric(vertical: 10),
+          ),
+          Container(
+            color: Colors.red,
+            width: double.infinity,
+            height: 100,
+            margin: const EdgeInsets.symmetric(vertical: 10),
+          ),
+        ],
+      ),
     );
   }
 }
