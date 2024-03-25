26ac4131bf1a2088f78a87e516c558d345919417
jerry <becomegoodguy@gmail.com>
2024-03-26 00:03:41 +0900

    widget 비율로 배치-1

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Widget을 상하좌우로 배치하기'),
+          title: const Text('Widget을 비율로 배치하기'),
         ),
         body: const Body(),
       ),
@@ -18,55 +20,50 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      // 화면 스크롤 가능
-      scrollDirection: Axis.horizontal,
-      child: Row(
-        children: [
-          Container(
-            color: Colors.red,
-            width: 100,
-            height: 100,
-            margin: const EdgeInsets.symmetric(horizontal: 10),
+    return Row(
+      children: [
+        Container(
+          color: Colors.red,
+          height: 200,
+          width: 50,
+        ),
+        Expanded(
+          child: Container(
+            color: Colors.blue,
+            height: 100, // 안됨.
           ),
-          Container(
-            color: Colors.red,
-            width: 100,
+        ),
+        Flexible(
+          child: Container(
+            color: Colors.yellow,
             height: 100,
-            margin: const EdgeInsets.symmetric(horizontal: 10),
           ),
-          Container(
-            color: Colors.red,
-            width: 100,
-            height: 100,
-            margin: const EdgeInsets.symmetric(horizontal: 10),
-          ),
-          Container(
-            color: Colors.red,
-            width: 100,
-            height: 100,
-            margin: const EdgeInsets.symmetric(horizontal: 10),
-          ),
-          Container(
-            color: Colors.red,
-            width: 100,
-            height: 100,
-            margin: const EdgeInsets.symmetric(horizontal: 10),
-          ),
-          Container(
-            color: Colors.red,
-            width: 100,
-            height: 100,
-            margin: const EdgeInsets.symmetric(horizontal: 10),
-          ),
-          Container(
-            color: Colors.red,
-            width: 100,
-            height: 100,
-            margin: const EdgeInsets.symmetric(horizontal: 10),
-          ),
-        ],
-      ),
+        )
+        // Flexible(
+        //   flex: 1,
+        //   child: Container(
+        //     color: Colors.red,
+        //   ),
+        // ),
+        // Flexible(
+        //   flex: 2,
+        //   child: Container(
+        //     color: Colors.blue,
+        //   ),
+        // ),
+        // Flexible(
+        //   flex: 3,
+        //   child: Container(
+        //     color: Colors.green,
+        //   ),
+        // ),
+        // Flexible(
+        //   flex: 4,
+        //   child: Container(
+        //     color: Colors.yellow,
+        //   ),
+        // ),
+      ],
     );
   }
 }
