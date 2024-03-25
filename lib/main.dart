1d7a24c8b61b5ffd330c019b4cc5ab1c87635893
jerry <becomegoodguy@gmail.com>
2024-03-25 23:43:03 +0900

    widget 상하좌우 배치

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
     MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Widget을 좌우로 배치하기'),
+          title: const Text('Widget을 상하좌우로 배치하기'),
         ),
         body: const Body(),
       ),
@@ -18,35 +18,42 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      height: double.infinity,
-      // child: Column(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center, // ROW의 가운데 정렬
-        crossAxisAlignment: CrossAxisAlignment
-            .center, // Column과 마찬가지로 Container의 height를 줘야 정렬이 됨.
-        children: [
-          Container(
-            width: 100,
-            height: 80,
-            color: Colors.red,
-            child: const Text('Container 1'),
-          ),
-          Container(
-            width: 100,
-            height: 80,
-            color: Colors.blue,
-            child: const Text('Container 2'),
-          ),
-          Container(
-            width: 100,
-            height: 80,
-            color: Colors.green,
-            child: const Text('Container 3'),
-          ),
-        ],
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start, // ROW의 가운데 정렬
+          crossAxisAlignment: CrossAxisAlignment
+              .center, // Column과 마찬가지로 Container의 height를 줘야 정렬이 됨.
+          children: [
+            Container(
+              width: 100,
+              height: 80,
+              color: Colors.red,
+              child: const Text('Container 1'),
+            ),
+            Container(
+              width: 100,
+              height: 80,
+              color: Colors.blue,
+              child: const Text('Container 2'),
+            ),
+            Container(
+              width: 100,
+              height: 80,
+              color: Colors.green,
+              child: const Text('Container 3'),
+            ),
+          ],
+        ),
+        Container(
+          width: 300,
+          height: 120,
+          color: Colors.yellow,
+          child: const Text("Container 4"),
+        )
+      ],
     );
   }
 }
