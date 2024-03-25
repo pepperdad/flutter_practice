3fe31faa04bea31de574350e50fd2d576a7ec782
jerry <becomegoodguy@gmail.com>
2024-03-25 23:07:05 +0900

    widget 상하 배치

--- lib/main.dart
+++ lib/main.dart
@@ -18,34 +18,31 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 300,
-      color: Colors.grey,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center, // y축 정렬
-        crossAxisAlignment: CrossAxisAlignment.end, // x축 정렬
-        children: [
-          Container(
-            width: 100,
-            height: 80,
-            color: Colors.red,
-            child: const Text('Container 1'),
-          ),
-          Container(
-            width: 100,
-            height: 80,
-            color: Colors.blue,
-            child: const Text('Container 2'),
-          ),
-          Container(
-            width: 100,
-            height: 80,
-            color: Colors.green,
-            child: const Text('Container 3'),
-          ),
-        ],
-      ),
+    return Column(
+      mainAxisSize: MainAxisSize.min, // Column의 크기를 최소로 설정
+      mainAxisAlignment: MainAxisAlignment.center, // y축 정렬
+      crossAxisAlignment: CrossAxisAlignment
+          .center, // x축 정렬 => column은 좌우 폭 제한 x => center로 정렬하고 싶으면 container로 감싸고 width를 지정해야 함
+      children: [
+        Container(
+          width: 100,
+          height: 80,
+          color: Colors.red,
+          child: const Text('Container 1'),
+        ),
+        Container(
+          width: 100,
+          height: 80,
+          color: Colors.blue,
+          child: const Text('Container 2'),
+        ),
+        Container(
+          width: 100,
+          height: 80,
+          color: Colors.green,
+          child: const Text('Container 3'),
+        ),
+      ],
     );
   }
 }
